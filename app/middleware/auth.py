ce11b523c7f42e9ac7c5150412d0bb98a723b716
Heisenberg1719 <heisenberg@khazanapay.in>
2024-10-13 20:21:51 +0000

    check

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,8 +1,7 @@
 import logging
 from flask_jwt_extended import *
-from flask import request, jsonify ,make_response
+from flask import request, jsonify 
 from datetime import timedelta
-
 
 def jwt_required_middleware():
     allowed_paths = ['/', '/user/login', '/admin/admin_login', '/user/logout', '/admin/logout']
@@ -17,8 +16,7 @@ def jwt_required_middleware():
                 return jsonify({"msg": "Unauthorized role access"}), 403
 
             new_access_token = create_access_token(identity=current_user,additional_claims={"role": claims.get("role")},expires_delta=timedelta(minutes=5))
-            set_access_cookies(response, new_access_token)
-            return response
+            request.new_access_token = new_access_token
         except Exception as e:
             logging.error(f"Error during JWT verification: {str(e)}", exc_info=True)
             request.jwt_verification_failed = True
